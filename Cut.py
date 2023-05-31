c57a7fe61683638a607d14a6ed78de8d36db23e9
ktronix97 <daliaestefany.1701421354@ucaldas.edu.co>
2023-05-31 12:18:36 -0500

    Modelos archivos de corte y rotacion v2

---
+++ Cut.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 14:44:14 2022
+
+@author: luisd
+"""
+import cv2
+
+
+# class to handle cropping options
+
+
+class Cut:
+    def __init__(self):
+        pass
+
+    # function to crop image
+    # Parameters: image to crop, contour, and the image number
+    def crop(self, image, contours, num):        
+        # cycles through all the contours to crop all
+        for c in contours:
+            # creates an approximate rectangle around contour
+            x, y, w, h = cv2.boundingRect(c)
+            # Only crop decently large rectangles
+            
+                # pulls crop out of the image based on dimensions
+            new_img = image[y:y + h, x:x + w]
+                # writes the new file in the Crops folder
+                # cv2.imwrite('Crops/' + 'crop_' + str(num) +
+                #             '.png', new_img)
+            # returns a number incremented up for the next file name
+            return new_img
